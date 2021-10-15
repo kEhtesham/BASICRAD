47ebe7824343124e17411b44338d4526e72055d9
kEhtesham <ehtesham.webosmotic@gmail.com>
2021-10-15 18:59:00 +0530

    Functionality of Adding Filter With Bug

--- JS/script.js
+++ JS/script.js
@@ -37,6 +37,10 @@ function myFunction() {
     for (k = 0; k < accordion_comp.length; k++) {
         accordion_comp[k].addEventListener("click", function() {
         this.classList.toggle("active");
+        // if(this.hasClass == "active"){
+        //     alert('active');
+        //     console.log('test');
+        // }
         var panel = this.nextElementSibling;
         if (panel.style.display === "block") {
             panel.style.display = "none";
@@ -44,8 +48,31 @@ function myFunction() {
             panel.style.display = "block";
         }
         });
-    }   
+    }
+    // var accordion_comp_active = document.querySelectorAll(".acc_heading.active");
+    
+    // for(i=0; i < accordion_comp_active.length; i++) {
+    //     var totalHeight = accordion_comp_active[i].getBoundingClientRect().height;
+    //     totalHeight = 78 * accordion_comp_active.length;
+    //     console.log(totalHeight);
+    // }
+    // console.log(document.querySelector('body').getBoundingClientRect().height);
 }
+
+
+//Accordion height on click
+
+// function accordionheightFunction() {
+//     var accordion_comp_active = document.querySelectorAll(".acc_heading.active");
+//     var accordion_elem = document.getElementsByClassName('accordion_elem');
+
+//     for(i=0; i < accordion_comp_active.length; i++) {
+//         var totalHeight = accordion_comp_active[i].getBoundingClientRect().height;
+//         totalHeight = totalHeight + 26;
+//         accordion_elem[i].style.height = totalHeight + "px";
+//         console.log(accordion_elem);
+//     }
+// }
 
 // Dropdown accordion 
 
@@ -84,4 +111,40 @@ function expandfiltrFunction(event, className) {
     for (i = 0; i < acc_filtr_block.length; i++) {
         acc_filtr_block[i].style.display = "block";
     }
+}
+
+
+// filter append
+
+function filterpushFunction() {
+    var all_filter_listing = document.getElementsByClassName('all_filter_listing');
+    var filterHeading = document.querySelectorAll('.getComponent');
+    
+        // var filterHeadingThis = filterHeading[i];
+    
+        var filter_elem = document.createElement("div");
+        filter_elem.setAttribute('class','filter_elem');
+            var filtr_options = document.createElement("div");
+            filtr_options.setAttribute('class','filtr_options');
+            for(i=0; i<filterHeading.length; i++) {
+                filterHeading[i].addEventListener('click', function() {
+                    // console.clear();
+                    var element_content = this.innerHTML;
+                    filtr_options.appendChild(document.createTextNode(element_content));
+                });
+            }
+            
+        filter_elem.appendChild(filtr_options);
+            var total_filter = document.createElement("div");
+            total_filter.setAttribute('class','total_filter');
+            total_filter.appendChild(document.createTextNode('(All)'));
+        filtr_options.appendChild(total_filter);
+            var caret_down = document.createElement("i");
+            caret_down.setAttribute('class','fas fa-caret-down');
+        filtr_options.appendChild(caret_down);
+            var filteration_options = document.createElement("div");
+            filteration_options.setAttribute('class','filteration_options');
+        filter_elem.appendChild(filteration_options);
+    all_filter_listing[0].appendChild(filter_elem);
+    // console.log(filter_elem);
 }
