118695ca129fb95635de114f400466878b88c317
kEhtesham <ehtesham.webosmotic@gmail.com>
2021-10-16 17:29:30 +0530

    Functionaly of Addfilter Added

--- JS/script.js
+++ JS/script.js
@@ -116,35 +116,98 @@ function expandfiltrFunction(event, className) {
 
 // filter append
 
-function filterpushFunction() {
-    var all_filter_listing = document.getElementsByClassName('all_filter_listing');
-    var filterHeading = document.querySelectorAll('.getComponent');
-    
-        // var filterHeadingThis = filterHeading[i];
-    
-        var filter_elem = document.createElement("div");
-        filter_elem.setAttribute('class','filter_elem');
-            var filtr_options = document.createElement("div");
-            filtr_options.setAttribute('class','filtr_options');
-            for(i=0; i<filterHeading.length; i++) {
-                filterHeading[i].addEventListener('click', function() {
-                    // console.clear();
-                    var element_content = this.innerHTML;
-                    filtr_options.appendChild(document.createTextNode(element_content));
-                });
-            }
+function filterpushFunction(func_ID) {
+    // debugger
+    var FilterAdded = document.getElementById(func_ID);
+    if (FilterAdded.checked == true) {
+        var all_filter_listing = document.getElementsByClassName('all_filter_listing');
+        var input_loop = document.getElementById(func_ID).step;
+        
+            var filter_elem = document.createElement("div");
+            filter_elem.setAttribute('class','filter_elem');
+                var filtr_options = document.createElement("div");
+                filtr_options.setAttribute('class','filtr_options');
+
+                var element_content = document.getElementById(func_ID).value;
+                // console.log(func_ID);
+                filtr_options.appendChild(document.createTextNode(element_content));
+
+            filter_elem.appendChild(filtr_options);
+                var total_filter = document.createElement("div");
+                total_filter.setAttribute('class','total_filter');
+                total_filter.appendChild(document.createTextNode('(All)'));
+            filtr_options.appendChild(total_filter);
+                var caret_down = document.createElement("i");
+                caret_down.setAttribute('class','fas fa-caret-down');
+            filtr_options.appendChild(caret_down);
+                var filteration_options = document.createElement("div");
+                filteration_options.setAttribute('class','filteration_options');
+            filter_elem.appendChild(filteration_options);
+
+        if (FilterAdded.classList.contains('dropdown_push')){
+                
+                // filteration_options
+
+                    var selectall_filtr = document.createElement("div");
+                    selectall_filtr.appendChild(document.createTextNode('Select All'));
+                    selectall_filtr.setAttribute('class','select_all');
+
+                    filteration_options.appendChild(selectall_filtr);
+
+                    var selectnone_filtr = document.createElement("div");
+                    selectnone_filtr.appendChild(document.createTextNode('Select None'));
+                    selectnone_filtr.setAttribute('class','select_none');
+
+                    filteration_options.appendChild(selectnone_filtr);
+                
+                    var inner_filtr_wrap = document.createElement("div");
+                    inner_filtr_wrap.setAttribute('class','filtr_wrap');
             
-        filter_elem.appendChild(filtr_options);
-            var total_filter = document.createElement("div");
-            total_filter.setAttribute('class','total_filter');
-            total_filter.appendChild(document.createTextNode('(All)'));
-        filtr_options.appendChild(total_filter);
-            var caret_down = document.createElement("i");
-            caret_down.setAttribute('class','fas fa-caret-down');
-        filtr_options.appendChild(caret_down);
-            var filteration_options = document.createElement("div");
-            filteration_options.setAttribute('class','filteration_options');
-        filter_elem.appendChild(filteration_options);
-    all_filter_listing[0].appendChild(filter_elem);
-    // console.log(filter_elem);
+                    filteration_options.appendChild(inner_filtr_wrap);
+
+                    for (k=0; k<input_loop; k++) {
+                        var filtr_wrap_parent = document.createElement("div");
+                        filtr_wrap_parent.setAttribute('class','filtr_element_wrap');
+
+                        inner_filtr_wrap.appendChild(filtr_wrap_parent);
+
+                        var input_innerFilter = document.createElement("input");
+                        
+                        var element_array = ["Maxim", "AVX", "Yageo"];
+                        input_innerFilter.setAttribute('type','checkbox');
+                        input_innerFilter.setAttribute('id', 'id'+k);
+                        filtr_wrap_parent.appendChild(input_innerFilter);
+
+                        var label_innerFilter = document.createElement("label");
+                        label_innerFilter.setAttribute('for','id'+k);
+                        label_innerFilter.innerHTML=element_array[k];
+                        filtr_wrap_parent.appendChild(label_innerFilter);
+                    }
+
+                    console.log(filteration_options);
+
+            all_filter_listing[0].appendChild(filter_elem);
+        }
+        
+        else {
+            var range_innerFilter = document.createElement("input");
+            range_innerFilter.setAttribute('type','range');
+            filteration_options.appendChild(range_innerFilter);
+            filter_elem.appendChild(filteration_options);
+            all_filter_listing[0].appendChild(filter_elem);
+            console.log(all_filter_listing[0]);
+        }
+    }
+    else {
+        console.log('its not a dropdown');
+    }
 }
+
+
+// filteration_options
+
+
+/*
+    <input value="500" min="500" max="50000" step="500" type="range" onchange="movedFunction()" id="first_value">
+    <input value="50000" min="500" max="50000" step="500" type="range" onchange="movedFunction()" id="last_value">
+*/
